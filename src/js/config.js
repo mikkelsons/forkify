7df1727e29584d06711c936606fd1b4b9a936d41
mikkelsons <mikkelson_shawn@yahoo.com>
2025-05-11 15:20:42 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SECONDS = 10;
+export const RESULTS_PER_PAGE = 10;
+export const API_KEY = '5f632e82-097f-42e8-8cfe-dcaf09dc3ce8';
+export const MODAL_CLOSE_SECONDS = 2.5;
